7db98cf906c736e8df45e2bb64c809843245f968
Azaliya <a_khakimyanova@mail.ru>
2024-12-20 17:28:46 +0500

    Отредаченный parser.py

--- parser.py
+++ parser.py
@@ -4,10 +4,8 @@ import calendar
 import sqlite3
 from functools import wraps
 
-authors = {}
-
-DataBase = "authors.db"
-
+all_authors = {}
+month_authors = {}
 
 def get_poem(author, link):
     url = "https://stihi.ru" + link
@@ -17,84 +15,106 @@ def get_poem(author, link):
     soup = BeautifulSoup(response.text, 'html.parser')
     rows = soup.find_all("a", class_='poemlink')
 
-    connection = sqlite3.connect("poems_base")
+    connection = sqlite3.connect("DataBase.db")
     cursor = connection.cursor()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Poems (
     id INTEGER PRIMARY KEY,
-    "article" TEXT NOT NULL,
-    "author" TEXT NOT NULL,
-    "text" TEXT NOT NULL
+    article TEXT NOT NULL,
+    author TEXT NOT NULL,
+    text TEXT NOT NULL
+    FOREIGN KEY (author) REFERENCES Authors (id)
     )
     ''')
     for row in rows:
-        article = row.get_text()
-        poem_link = "https://stihi.ru" + row.get('href')
-        print(poem_link)
-        response1 = requests.get(poem_link)
-        response1.raise_for_status()
+        try:
+            article = row.get_text()
+            poem_link = "https://stihi.ru" + row.get('href')
+            print(poem_link)
+            response1 = requests.get(poem_link)
+            response1.raise_for_status()
 
-        soup1 = BeautifulSoup(response1.text, 'html.parser')
-        row1 = soup1.find("div", class_='text')
-        poem = row1.get_text()
+            soup1 = BeautifulSoup(response1.text, 'html.parser')
+            row1 = soup1.find("div", class_='text')
+            poem = row1.get_text()
 
-        cursor.execute('INSERT INTO Poems (article, author, text) VALUES (?, ?, ?)', (article, author, poem))
-
+            cursor.execute('INSERT INTO Poems (article, author, text) VALUES (?, ?, ?)', (article, author, poem))
+        except Exception:
+            continue
+    #     https://stihi.ru/2024/01/21/7507
     connection.commit()
     connection.close()
     # print(response.text)
 
 
 
-def get_author(url, authors):
+def get_author(url, month_authors):
     print(url)
     response = requests.get(url)
     response.raise_for_status()
+
+    connection = sqlite3.connect("DataBase.db")
+    cursor = connection.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Authors (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+    )
+    ''')
+
     soup = BeautifulSoup(response.text, 'html.parser')
     rows = soup.find_all("a", class_='recomlink')
     for row in rows:
         author = row.get_text()
         link = row.get('href')
-        if author not in authors:
-            authors[author] = 1
-            # get_poem(author, link)
-        else:
-            authors[author] += 1
-    # return authors
-    # hrefs = topics.find_all('a')
 
+        if author not in all_authors:
+            cursor.execute('INSERT INTO Authors (name) VALUES ("author") RETURNING id')
+            author_id = cursor.fetchall()
+            get_poem(author_id, link)
 
-connection = sqlite3.connect("poems_base.db")
-cursor = connection.cursor()
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS best_month (
-id INTEGER PRIMARY KEY,
-month_num INTEGER,
-"author" TEXT NOT NULL,
-"rang" INTEGER NOT NULL
-)
-''')
+            if author not in month_authors:
+                month_authors[author_id] = 1
+            else:
+                month_authors[author_id] += 1
 
-for i in range(2024, 2025):
-    for j in range(1, 3):
-        days = calendar.monthrange(i, j)
-        if j < 10:
-            j = "0" + str(j)
-        k = 1
-        for day in range(days[1]):
-            get_author(f"https://stihi.ru/authors/editor.html?year={i}&month={j}&day={day + 1}", authors)
+    connection.commit()
+    connection.close()
 
-        for author, rang in authors.items():
-            cursor.execute('INSERT INTO best_month (month_num, author, rang) VALUES (?, ?, ?)', (j, author, rang))
+def get_best_month():
+    connection = sqlite3.connect("DataBase.db")
+    cursor = connection.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS BestMonth (
+    id INTEGER PRIMARY KEY,
+    month_num INTEGER,
+    author_id TEXT NOT NULL,
+    rang INTEGER NOT NULL
+    FOREIGN KEY (author_id) REFERENCES Authors (id)
+    )
+    ''')
 
-        connection.commit()
-        authors = {}
-connection.close()
+    for year in range(2024, 2025):
+        for month in range(1, 3):
+            days = calendar.monthrange(year, month)
+            if month < 10:
+                month = "0" + str(month)
+            k = 1
+            for day in range(days[1]):
+                get_author(f"https://stihi.ru/authors/editor.html?year={year}&month={month}&day={day + 1}", month_authors)
 
+            for author_id, rang in month_authors.items():
+                cursor.execute('INSERT INTO BestMonth (month_num, author_id, rang) VALUES (?, ?, ?)', (month, author_id, rang))
 
-connection = sqlite3.connect("poems_base.db")
-cursor = connection.cursor()
-cursor.execute('''SELECT * FROM best_month ''')
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+            month_authors = {}
+
+    connection.commit()
+    connection.close()
+
+#
+# connection = sqlite3.connect("DataBase.db")
+# cursor = connection.cursor()
+# cursor.execute('''SELECT * FROM BestMonth ''')
+# rows = cursor.fetchall()
+# for row in rows:
+#     print(row)
