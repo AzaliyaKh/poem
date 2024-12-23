2c458f26e7d1e5fa1d85a9d507d27dedd2360d67
Azaliya <a_khakimyanova@mail.ru>
2024-12-23 23:53:51 +0500

    Added req.txt and build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+POEM="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+docker-compose -f "${POEM}/docker-compose.yml" down
+docker-compose -f "${POEM}/docker-compose.yml" build
+docker-compose -f "${POEM}/docker-compose.yml" up
